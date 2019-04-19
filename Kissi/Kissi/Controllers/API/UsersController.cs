77a9e249829f0929186b41ce023ecdcfa8b36e6d
Heritier Luthala <36164495+hluthala@users.noreply.github.com>
2019-04-19 22:55:37 +0100

    Login
    Api

--- Kissi/Kissi/Controllers/API/UsersController.cs
+++ Kissi/Kissi/Controllers/API/UsersController.cs
@@ -1,20 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
 using Kissi.Models;
+using Kissi.Classes;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Newtonsoft.Json.Linq;
 
 namespace Kissi.Controllers.API
 {
+    [RoutePrefix("api/Users")]
     public class UsersController : ApiController
     {
         private KissiContext db = new KissiContext();
+
+        [HttpPost]
+        [Route("Login")]
+        public IHttpActionResult Login(JObject form)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            string email = string.Empty;
+            string password = string.Empty;
+            dynamic jsonObject = form;
+
+            try
+            {
+                email = jsonObject.Email.Value;
+                password = jsonObject.Password.Value;
+            }
+            catch
+            {
+                return this.BadRequest("Incorrect call");
+            }
+
+            var userContext = new ApplicationDbContext();
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(userContext));
+            var userASP = userManager.Find(email, password);
+
+            if (userASP == null)
+            {
+                return this.BadRequest("User or password wrong");
+            }
+
+            var user = db.Users.Where(u => u.UserName == email).Include(u => u.Company).Include(u => u.City).Include(u => u.Department).FirstOrDefault();
+
+            if (user == null)
+            {
+                return this.BadRequest("User not found");
+            }
+
+            var userResponse = new UserResponse
+            {
+                Address = user.Address,
+                CityId = user.CityId,
+                CityName = user.City.Name,
+                Company = user.Company,
+                DepartmentId = user.DepartmentId,
+                DepartmentName = user.Department.Name,
+                FirstName = user.FirstName,
+                IsAdmin = userManager.IsInRole(userASP.Id, "Admin"),
+                IsCustomer = userManager.IsInRole(userASP.Id, "Customer"),
+                IsSupplier = userManager.IsInRole(userASP.Id, "Supplier"),
+                IsUser = userManager.IsInRole(userASP.Id, "User"),
+                LastName = user.LastName,
+                Phone = user.Phone,
+                Photo = user.Photo,
+                UserId = user.UserId,
+                UserName = user.UserName,
+            };
+
+            return this.Ok(userResponse);
+        }
 
         // GET: api/Users
         public IQueryable<User> GetUsers()
